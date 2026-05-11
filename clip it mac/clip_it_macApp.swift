5f5aa8d2ffe1b736ec5c62d2de16e2a451628e02
itzadetunji <adetunjiadeyinka29@gmail.com>
2026-05-11 22:36:18 +0100

    Initial Commit

---
+++ clip it mac/clip_it_macApp.swift
@@ -0,0 +1,17 @@
+//
+//  clip_it_macApp.swift
+//  clip it mac
+//
+//  Created by Adetunji Adeyinka on 11/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct clip_it_macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
